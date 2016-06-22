7f77a6f9abf7f4d10aa512e8badc62e74c47fd5e
Alexander James Turinke <a.turinske@gmail.com>
2016-06-22 16:01:31 -0700

    Implement psuedocode for islands

--- islands.js
+++ islands.js
@@ -16,5 +16,17 @@
  */
 
 var findIslands = function(matrix) {
-
+  // define an array, tupleArray, to store the position of the land
+  // define an array, queue, to act as a queue
+  // define counter to count the number of islands
+  // define check as the tuple one is checking against
+  // traverse the matrix and create a tuple holding the information of the [y, x] and store it in tupleArray
+  // for every element in tupleArray
+    // remove the first element from the tupleArray and place it in the queue
+    // increment counter by one
+    // for every element in queue
+      // remove the first element from the queue and set check equal to it
+      // for every element in tupleArray
+        // if any other tuple is near check, place them in the queue
+  // return counter
 };
