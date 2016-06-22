314d96fce7d3a8d05f9ea18a824e11b0cf3ecca4
Alexander James Turinke <a.turinske@gmail.com>
2016-06-22 16:01:10 -0700

    Implement skeleton for island problem

---
+++ islands.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Given a matrix where 1s represent land and 0s represent water, determine how
+ * many islands there are in the ocean where 1s next to each other are considered
+ * one island
+ *
+ * EXAMPLE
+ * var test = [[1, 0, 0, 0],
+ *             [1, 0, 1, 0],
+ *             [0, 0, 0, 1],
+ *             [0, 1, 0, 0]]
+ *
+ * console.log(findIslands(test));
+ * //-> 3
+ */
+
+var findIslands = function(matrix) {
+
+};
