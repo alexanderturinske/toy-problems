4fcc65b31b9abbc28bfe0f51cd88c85f9c1cfb2b
Alexander James Turinke <a.turinske@gmail.com>
2016-06-22 16:52:07 -0700

    Start to implement function. There is an issue where it doesn't return the right number

--- islands.js
+++ islands.js
@@ -17,16 +17,56 @@
 
 var findIslands = function(matrix) {
   // define an array, tupleArray, to store the position of the land
+  var tupleArray = [];
   // define an array, queue, to act as a queue
+  var queue = [];
   // define counter to count the number of islands
+  var counter = 0;
   // define check as the tuple one is checking against
+  var check = [];
   // traverse the matrix and create a tuple holding the information of the [y, x] and store it in tupleArray
+  matrix.forEach(function (e, j) {
+    e.forEach(function (el, i) {
+      if (el) {
+        tupleArray.push([j, i]);
+      }
+    });
+  });
   // for every element in tupleArray
+  tupleArray.forEach(function (ele, k) {
     // remove the first element from the tupleArray and place it in the queue
+    queue.push(tupleArray.shift());
     // increment counter by one
+    counter++;
     // for every element in queue
+    queue.forEach(function (elem) {
       // remove the first element from the queue and set check equal to it
+      check = queue.shift();
       // for every element in tupleArray
+      tupleArray.forEach(function (eleme, l) {
         // if any other tuple is near check, place them in the queue
+        if (eleme[0] + 1 === check[0] && eleme[1] + 1 === check[1] ||
+          eleme[0] + 1 === check[0] && eleme[1] === check[1] ||
+          eleme[0] + 1 === check[0] && eleme[1] - 1 === check[1] ||
+          eleme[0] === check[0] && eleme[1] + 1 === check[1] ||
+          eleme[0] === check[0] && eleme[1] - 1 === check[1] ||
+          eleme[0] - 1 === check[0] && eleme[1] + 1 === check[1] ||
+          eleme[0] - 1 === check[0] && eleme[1] === check[1] ||
+          eleme[0] - 1 === check[0] && eleme[1] - 1 === check[1]
+        ) {
+          queue.push(elem);
+          tupleArray = tupleArray.splice(l, 1);
+        }
+      });
+    });
+  });
   // return counter
+  return counter;
 };
+
+var test = [[1, 0, 0, 0],
+            [1, 0, 1, 0],
+            [0, 0, 0, 1],
+            [0, 0, 1, 0]];
+
+console.log(findIslands(test));
