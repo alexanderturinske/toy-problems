9ef7441153ad993d46208dba716b5acd29285487
Alexander James Turinke <a.turinske@gmail.com>
2016-06-22 18:32:34 -0700

    Build skeleton of rps function

---
+++ rps.js
@@ -0,0 +1,17 @@
+'use strict';
+/**
+ * Implement a function that lists out all possible ways one can play n rounds of
+ * the game rock, paper, sciccors
+ *
+ * EXAMPLE:
+ * console.log(rps(2));
+ * //-> [[r, r], [r, p], [r, s], [p, r], [p, p], [p, s], [s, r], [s, p], [s, s]]
+ */
+
+var rps = function (n) {
+
+};
+
+module.exports = {
+  rps: rps,
+};
