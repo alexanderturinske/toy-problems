af957d72dd0be3419e156c2e4a6bbadf6f39628a
Alexander James Turinke <a.turinske@gmail.com>
2016-06-25 12:12:58 -0700

    Implement isPrime recursively

--- Numbers/isPrime.js
+++ Numbers/isPrime.js
@@ -3,21 +3,41 @@
  * Determine if the number is prime
 */
 
+// Iterative
+// var isPrime = function (number) {
+//   if (number <= 0) {
+//     return 'Invalid entry!';
+//   }
+//   if (number === 1 || number === 2) {
+//     return true;
+//   }
+//   for (var i = 2; i <= Math.floor(number / 2); i++) {
+//     if (!(number % i)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// Recursively
 var isPrime = function (number) {
-  if (number <= 0) {
+  var test = arguments[1] || Math.floor(number / 2);
+  // base cases
+  if (number < 1) {
     return 'Invalid entry!';
   }
-  if (number === 1 || number === 2) {
+  if (test < 2) {
     return true;
   }
-  for (var i = 2; i <= Math.floor(number / 2); i++) {
-    if (!(number % i)) {
-      return false;
-    }
+  if (!(number % test)) {
+    return false;
   }
-  return true;
+  // recursive case
+  return isPrime(number, test - 1);
 };
 
+console.log(isPrime(-200));
+//-> 'Invalid entry!'
 console.log(isPrime(-1));
 //-> 'Invalid entry!'
 console.log(isPrime(0));
@@ -29,6 +49,8 @@ console.log(isPrime(2));
 console.log(isPrime(3));
 //-> true
 console.log(isPrime(4));
+//-> false
+console.log(isPrime(80));
 //-> false
 console.log(isPrime(400));
 //-> false
