aec9b9f0e8fdb13a03e80f821b39f7c057862acb
Alexander James Turinke <a.turinske@gmail.com>
2016-06-25 16:40:11 -0700

    Implement an iterative solution

--- Numbers/nthFibonacci.js
+++ Numbers/nthFibonacci.js
@@ -3,15 +3,28 @@
  * Find the nth Fibonacci number
 **/
 
+// Iterative
 var nthFibonacci = function (n) {
+  var lastTwo = [0, 1];
   if (n === 0) {
     return 0;
   }
-  if (n <= 2) {
-    return 1;
+  for (var i = 2; i <= n; i++) {
+    lastTwo.push(lastTwo.shift(0) + lastTwo[0]);
   }
-  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+  return lastTwo[1];
 };
+
+// Recursive
+// var nthFibonacci = function (n) {
+//   if (n === 0) {
+//     return 0;
+//   }
+//   if (n <= 2) {
+//     return 1;
+//   }
+//   return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+// };
 
 console.log(0 === nthFibonacci(0));
 //-> 0
