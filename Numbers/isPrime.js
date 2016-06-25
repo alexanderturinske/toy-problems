b3171bd56e42278c910b0c24b2a9275f33db54c3
Alexander James Turinke <a.turinske@gmail.com>
2016-06-25 12:04:53 -0700

    Implement isPrime iteratively

--- Numbers/isPrime.js
+++ Numbers/isPrime.js
@@ -3,9 +3,35 @@
  * Determine if the number is prime
 */
 
-var isPrime = function () {
-
+var isPrime = function (number) {
+  if (number <= 0) {
+    return 'Invalid entry!';
+  }
+  if (number === 1 || number === 2) {
+    return true;
+  }
+  for (var i = 2; i <= Math.floor(number / 2); i++) {
+    if (!(number % i)) {
+      return false;
+    }
+  }
+  return true;
 };
+
+console.log(isPrime(-1));
+//-> 'Invalid entry!'
+console.log(isPrime(0));
+//-> 'Invalid entry!'
+console.log(isPrime(1));
+//-> true
+console.log(isPrime(2));
+//-> true
+console.log(isPrime(3));
+//-> true
+console.log(isPrime(4));
+//-> false
+console.log(isPrime(400));
+//-> false
 
 module.exports = {
 isPrime: isPrime,
