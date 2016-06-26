2d9c7440c9d935e01e2ecc13bee8e46a3e1e701b
Alexander James Turinke <a.turinske@gmail.com>
2016-06-26 16:59:03 -0700

    Build skeleton of water blocks

--- Matricies/waterBlocks.js
+++ Matricies/waterBlocks.js
@@ -1,20 +1,25 @@
 'use strict';
 /**
- * Description
+ * There is a matrix of 1s and 0s where 1s represent blocks and 0s represent blocks
+ * If it rains, how many squares of water will there be?
 **/
 
-var waterBlocks = function () {
+var waterBlocks = function (matrix) {
 
 };
 
-console.log(waterBlocks());
-//-> EXPECTED OUTCOME
-console.log(waterBlocks());
-//-> EXPECTED OUTCOME
-console.log(waterBlocks());
-//-> EXPECTED OUTCOME
-console.log(waterBlocks());
-//-> EXPECTED OUTCOME
+console.log(waterBlocks([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) === EXPECTED OUTCOME);
+// -> EXPECTED OUTCOME
+console.log(waterBlocks([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) === EXPECTED OUTCOME);
+// -> EXPECTED OUTCOME
+console.log(waterBlocks([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) === EXPECTED OUTCOME);
+// -> EXPECTED OUTCOME
+console.log(waterBlocks([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) === EXPECTED OUTCOME);
+// -> EXPECTED OUTCOME
+console.log(waterBlocks([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) === EXPECTED OUTCOME);
+// -> EXPECTED OUTCOME
+console.log(waterBlocks([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) === EXPECTED OUTCOME);
+// -> EXPECTED OUTCOME
 
 module.exports = {
   waterBlocks: waterBlocks,
