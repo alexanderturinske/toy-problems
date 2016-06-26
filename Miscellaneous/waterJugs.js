8eb79ed2e82453b220262f4f87c6c5e808203168
Alexander James Turinke <a.turinske@gmail.com>
2016-06-26 09:43:41 -0700

    Build skeleton for water jugs

---
+++ Miscellaneous/waterJugs.js
@@ -0,0 +1,32 @@
+'use strict';
+/**
+You are given two jugs with capacities x and y litres. There is an infinite amount of water supply available. You need to determine whether it is possible to measure exactly z litres using these two jugs.
+
+If z liters of water is measurable, you must have z liters of water contained within one or both buckets by the end.
+
+Operations allowed:
+Fill any of the jugs completely with water.
+Empty any of the jugs.
+Pour water from one jug into another till the other jug is completely full or the first jug itself is empty.
+
+**/
+
+var waterJugs = function (jug1, jug2, desiredVolume) {
+};
+
+console.log(waterJugs(3, 5, 4) === true);
+// -> true
+console.log(waterJugs(2, 5, 13) === true);
+// -> true
+console.log(true === waterJugs(4, 5, 81));
+// -> true
+console.log(false === waterJugs(2, 6, 5));
+// -> false
+console.log(false === waterJugs(2, 4, 11));
+// -> false
+// console.log(false === waterJugs(8, 4, 111));
+// -> false
+
+module.exports = {
+waterJugs: waterJugs,
+};
