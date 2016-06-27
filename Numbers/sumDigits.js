30dd08a52b59c000a82cc393571b9f8cc1d043c4
Alexander James Turinke <a.turinske@gmail.com>
2016-06-26 17:18:04 -0700

    Implement a recursive solution

--- Numbers/sumDigits.js
+++ Numbers/sumDigits.js
@@ -1,10 +1,22 @@
 'use strict';
 /**
  * Sum all the digits of a number into a single digit number
+ * Try it recursively
+ * Try it iteratively
+ * Try it in constant time
 **/
 
+// Recursive
 var sumDigits = function (n) {
-
+  var nArr = typeof n === 'string' ? n.split('') : n.toString().split('');
+  // base case
+  if (nArr.length === 1) {
+    return parseInt(nArr[0]);
+  }
+  // recursive case
+  return sumDigits(nArr.reduce(function (accumulator, current) {
+    return accumulator + parseInt(current);
+  }, 0));
 };
 
 console.log(sumDigits(1) === 1);
