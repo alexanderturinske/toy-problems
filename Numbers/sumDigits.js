4da6039372a03f2560c76db3b0325f443f6f5805
Alexander James Turinke <a.turinske@gmail.com>
2016-06-26 17:51:02 -0700

    Implement it in time complexity of O(10)

--- Numbers/sumDigits.js
+++ Numbers/sumDigits.js
@@ -7,16 +7,41 @@
 **/
 
 // Recursive
-var sumDigits = function (n) {
-  var nArr = typeof n === 'string' ? n.split('') : n.toString().split('');
-  // base case
-  if (nArr.length === 1) {
-    return parseInt(nArr[0]);
+// var sumDigits = function (n) {
+//   var nArr = typeof n === 'string' ? n.split('') : n.toString().split('');
+//   // base case
+//   if (nArr.length === 1) {
+//     return parseInt(nArr[0]);
+//   }
+//   // recursive case
+//   return sumDigits(nArr.reduce(function (accumulator, current) {
+//     return accumulator + parseInt(current);
+//   }, 0));
+// };
+
+// Constant Time
+var sumDigits = function(n) {
+  if (n === 0) {
+    return 0
+  } else if (!((n - 1) % 9)) {
+    return 1;
+  } else if (!((n - 2) % 9)) {
+    return 2;
+  } else if (!((n - 3) % 9)) {
+    return 3;
+  } else if (!((n - 4) % 9)) {
+    return 4;
+  } else if (!((n - 5) % 9)) {
+    return 5;
+  } else if (!((n - 6) % 9)) {
+    return 6;
+  } else if (!((n - 7) % 9)) {
+    return 7;
+  } else if (!((n - 8) % 9)) {
+    return 8;
+  } else if (!((n - 9) % 9)) {
+    return 9;
   }
-  // recursive case
-  return sumDigits(nArr.reduce(function (accumulator, current) {
-    return accumulator + parseInt(current);
-  }, 0));
 };
 
 console.log(sumDigits(1) === 1);
