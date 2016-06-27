83fac5baa7dba9d7eab93fe87c8d2b9a9dc69690
Alexander James Turinke <a.turinske@gmail.com>
2016-06-27 14:28:44 -0700

    Implement longest palindrome

--- Strings/longestPalindrome.js
+++ Strings/longestPalindrome.js
@@ -4,7 +4,22 @@
 **/
 
 var longestPalindrome = function (string) {
+  var longest = arguments[1] === undefined ? string[0] : arguments[1];
+  var temp;
+  // base case
+  if (string.length <= longest.length) {
+    return longest;
+  }
 
+  for (var i = 0; string.substring(i).length > longest.length; i++) {
+    temp = string.substring(i).split('').reverse().join('');
+    if (temp === string.substring(i)) {
+      longest = temp;
+    }
+  }
+  // recursive case
+  longest = longestPalindrome(string.substring(0, string.length - 1), longest);
+  return longest;
 };
 
 console.log(longestPalindrome('doggod') === 'doggod');
