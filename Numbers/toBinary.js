137724f915c90bd3afef3cd2de0a83ee3117df59
Alexander James Turinke <a.turinske@gmail.com>
2016-06-29 16:42:21 -0700

    Implement psuedo code

--- Numbers/toBinary.js
+++ Numbers/toBinary.js
@@ -3,8 +3,20 @@
  * Create a function that converts a number in base 10 to base 2
 **/
 
-var toBinary = function () {
-
+var toBinary = function (n) {
+  // define number equal to n
+  // create array to hold numbers
+  // if number is odd, remove add 1 to array and remove
+  // create a counter starting at 1
+  // while remainder of number doesn't equal the number
+    // increment counter
+  // for the number of times of the counter
+    // if the remainder isn't the same as the number
+      // set number to the remainder
+      // unshift a one into the number array
+    // else
+      // unshift a zero
+  // return the array joined
 };
 
 console.log(toBinary(31) === '11111');
