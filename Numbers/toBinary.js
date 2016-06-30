d6b55c31d0947292d44b7582a40fefe173d4084d
Alexander James Turinke <a.turinske@gmail.com>
2016-06-29 17:04:28 -0700

    Implement toBinary

--- Numbers/toBinary.js
+++ Numbers/toBinary.js
@@ -5,18 +5,39 @@
 
 var toBinary = function (n) {
   // define number equal to n
+  var number = n;
   // create array to hold numbers
-  // if number is odd, remove add 1 to array and remove
+  var numArray = [];
+  // if number is odd
+  if (!(n % 2)) {
+    // remove add 1 to array and remove
+    numArray.push(1);
+    number--;
+  }
   // create a counter starting at 1
+  var counter = 1;
   // while remainder of number doesn't equal the number
+  while (number % Math.pow(2, counter) !== number) {
     // increment counter
+    counter++;
+  }
   // for the number of times of the counter
+  for (var i = 0; i < counter; i++) {
     // if the remainder isn't the same as the number
+    if (number % Math.pow(2, counter)) {
       // set number to the remainder
+      number = number % Math.pow(2, counter);
       // unshift a one into the number array
+      numArray.unshift(1);
     // else
+    } else {
       // unshift a zero
+      numArray.unshift(0);
+    }
+  }
+  console.log(numArray.join(''));
   // return the array joined
+  return numArray.join('');
 };
 
 console.log(toBinary(31) === '11111');
