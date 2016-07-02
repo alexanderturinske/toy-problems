ede33575a43c10d6d97740a39582ca941e066ef4
Alexander James Turinke <a.turinske@gmail.com>
2016-07-02 12:57:35 -0700

    Implement psuedocode

--- Numbers/permutations.js
+++ Numbers/permutations.js
@@ -1,10 +1,33 @@
 'use strict';
 /**
- * Description
+ * Return an array of all the different array permutations of the given numbers
+ *
+ * This is going to be a tree structure, which is recursive
+ *                                   R
+ *               num1           num2       num3    ...
+ *           num2  num3 ...    
+ *        num3 ....
+ *      ....
+ *     [num1, num2, num3, ...]
+ *
 **/
 
 var permutations = function (nums) {
-
+  // create a numsArray from the arguments
+  // instantiate empty array called allPerms
+  // instantiate inner function, createPerms, that takes currentPerm array
+    // instantiate currentPerms as either the array passed in or an empty array
+    // base case
+    // if currentPerms length is equal to numsArray length
+      // push currentPerms into allPerms
+      // exit function
+    // recursive case
+    // for every element in nums
+      // set curentPerms equal to the concat element to currentPerms
+      // call createPerms recursively with currentPerms
+      // set currentPerms equal to the sliced version of currentPerms that doesn't have the last element
+  // call createPerms
+  // return allPerms
 };
 
 console.log(permutations(1, 2) === [[1, 2], [2, 1]]);
