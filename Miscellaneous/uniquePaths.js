654c66abac2b1c4a8f2d95495d03f2ff989efebc
Alexander James Turinke <a.turinske@gmail.com>
2016-07-02 17:28:00 -0700

    Implement uniquePaths

--- Miscellaneous/uniquePaths.js
+++ Miscellaneous/uniquePaths.js
@@ -10,18 +10,32 @@ How many possible unique paths are there?
 
 var uniquePaths = function (m, n) {
   // instantiate counter equal to 0
+  var counter = 0;
   // instantiate inner function paths that takes a y and x parameter
+  var paths = function (y, x) {
     // base cases
     // if y is greater than m or x is greater than n
+    if (y === m || x === n) {
       // return out of the function
+      return;
+    }
     // if y equals m and x equals n
+    if (y === m - 1 && x === n - 1) {
       // increment counter
+      counter++;
       // return
+      return;
+    }
     // recursive case
     // call paths with y + 1, x
+    paths(y + 1, x);
     // call paths with y, x + 1
+    paths(y, x + 1);
+  };
   // call paths with m and n
+  paths(0, 0);
   // return counter
+  return counter;
 };
 
 console.log(uniquePaths(1, 2) === 1);
