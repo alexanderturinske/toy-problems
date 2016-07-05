f0fa775aeb987dec28e49d4b3340a0f2a2acf10e
Alexander James Turinke <a.turinske@gmail.com>
2016-07-05 10:37:11 -0700

    Implement skeleton of asyncmap

---
+++ Arrays/asyncMap.js
@@ -0,0 +1,21 @@
+'use strict';
+/**
+ * Description
+**/
+
+const asyncMap = () => {
+
+};
+
+console.log(asyncMap() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(asyncMap() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(asyncMap() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(asyncMap() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+
+module.exports = {
+  asyncMap,
+};
