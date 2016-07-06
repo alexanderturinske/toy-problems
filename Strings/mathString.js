d52ac0ba8150d44d4dc19c95d41d550ba0689b89
Alexander James Turinke <a.turinske@gmail.com>
2016-07-06 16:32:54 -0700

    Implement more tests

--- Strings/mathString.js
+++ Strings/mathString.js
@@ -4,18 +4,18 @@
  help: abstract syntax trees, recursive, eval
 **/
 
-const mathString = () => {
+const mathString = (string) => {
 
 };
 
 console.log(mathString('5+4*3-8') === 9);
 // -> 9
-console.log(mathString() === EXPECTED_OUTCOME);
-// -> EXPECTED_OUTCOME
-console.log(mathString() === EXPECTED_OUTCOME);
-// -> EXPECTED_OUTCOME
-console.log(mathString() === EXPECTED_OUTCOME);
-// -> EXPECTED_OUTCOME
+console.log(mathString('12*12/2+8') === 80);
+// -> 80
+console.log(mathString('12+12/2*8') === 96);
+// -> 96
+console.log(mathString('1000*9/3000') === 3);
+// -> 3
 
 module.exports = {
   mathString,
