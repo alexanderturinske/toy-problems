326d92fc12dbbbbb585320f1b702be9475cf50e3
Alexander James Turinke <a.turinske@gmail.com>
2016-07-06 09:42:57 -0700

    Implement skeleton for mathString

---
+++ Strings/mathString.js
@@ -0,0 +1,21 @@
+'use strict';
+/**
+ * Take a string equation (E.g., ‘5+4*3-8’) and calculate the result and write a test
+**/
+
+const mathString = () => {
+
+};
+
+console.log(mathString('5+4*3-8') === 19);
+// -> 19
+console.log(mathString() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(mathString() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(mathString() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+
+module.exports = {
+  mathString,
+};
