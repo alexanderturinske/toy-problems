9fadf6d8eb288ccab5effa5667c5b262c0a17253
Alexander James Turinke <a.turinske@gmail.com>
2016-07-06 11:01:20 -0700

    Implement skeleton of fileFinder

---
+++ Miscellaneous/fileFinder.js
@@ -0,0 +1,24 @@
+'use strict';
+/**
+create a simple command-line tool (using js/node) that searches inside a directory for all text files that have a specific word
+grep?
+process.exec?
+pass arguments?
+**/
+
+const fileFinder = () => {
+
+};
+
+console.log(fileFinder() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(fileFinder() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(fileFinder() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(fileFinder() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+
+module.exports = {
+  fileFinder,
+};
