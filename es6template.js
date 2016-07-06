3925cbf97de6e596a93db1eaa90ab21eaab4ddfb
Alexander James Turinke <a.turinske@gmail.com>
2016-07-05 19:15:43 -0700

    Implement es6 template

---
+++ es6template.js
@@ -0,0 +1,21 @@
+'use strict';
+/**
+ * Description
+**/
+
+const NAME = () => {
+
+};
+
+console.log(NAME() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(NAME() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(NAME() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(NAME() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+
+module.exports = {
+  NAME,
+};
