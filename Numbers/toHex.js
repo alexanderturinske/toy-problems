5e626940678166ce72744761cfe4fa9205c1d62f
Alexander James Turinke <a.turinske@gmail.com>
2016-07-06 10:04:43 -0700

    Implement skeleton for toHex

---
+++ Numbers/toHex.js
@@ -0,0 +1,24 @@
+'use strict';
+/**
+   Write a function that converts a number to hex
+   Example
+   (7).toString(16)
+   (10).toString(16)
+**/
+
+const toHex = () => {
+
+};
+
+console.log(toHex() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(toHex() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(toHex() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+console.log(toHex() === EXPECTED_OUTCOME);
+// -> EXPECTED_OUTCOME
+
+module.exports = {
+  toHex,
+};
