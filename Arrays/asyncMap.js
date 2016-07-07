d40fed6056c5c03c74761649f1d869ff9a55eeb2
Alexander James Turinke <a.turinske@gmail.com>
2016-07-07 13:21:55 -0700

    Implement async map

--- Arrays/asyncMap.js
+++ Arrays/asyncMap.js
@@ -3,18 +3,48 @@
  * Description
 **/
 
-const asyncMap = () => {
-
+const asyncMap = (tasks, callback) => {
+  const resultArray = [];
+  let counter = 0;
+  console.log(tasks);
+  for (let i = 0; i < tasks.length; i++) {
+    ((i) => {
+      tasks[i]((result) => {
+        resultArray[i] = result;
+        counter++;
+        if (counter === tasks.length) {
+          return callback(resultArray);
+        }
+      });
+    })(i);
+  }
 };
 
-console.log(asyncMap() === EXPECTED_OUTCOME);
-// -> EXPECTED_OUTCOME
-console.log(asyncMap() === EXPECTED_OUTCOME);
-// -> EXPECTED_OUTCOME
-console.log(asyncMap() === EXPECTED_OUTCOME);
-// -> EXPECTED_OUTCOME
-console.log(asyncMap() === EXPECTED_OUTCOME);
-// -> EXPECTED_OUTCOME
+console.log(asyncMap([
+ (cb) => {
+   setTimeout(() => {
+     cb('first');
+   }, 200);
+ },
+ (cb) => {
+   setTimeout(() => {
+     cb('second');
+   }, 100);
+ }
+],
+ (results) => {
+   console.log(results);
+   // ['first', 'second']
+}));
+
+// console.log(asyncMap() === 0);
+// // -> 0
+// console.log(asyncMap() === 0);
+// // -> 0
+// console.log(asyncMap() === 0);
+// // -> 0
+// console.log(asyncMap() === 0);
+// // -> EXPECTED_OUTCOME
 
 module.exports = {
   asyncMap,
