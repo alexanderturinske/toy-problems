7296b03190e2118fdef423b84d4a5fc28109a91f
Alexander James Turinke <a.turinske@gmail.com>
2016-07-10 11:19:26 -0700

    Implement asyncmap with let i instead of var i

--- Arrays/asyncMap.js
+++ Arrays/asyncMap.js
@@ -1,22 +1,37 @@
 'use strict';
 /**
- * Description
+ * Implement async map
 **/
 
+// const asyncMap = (tasks, callback) => {
+//   const resultArray = [];
+//   let counter = 0;
+//   console.log(tasks);
+//   for (let i = 0; i < tasks.length; i++) {
+//   // this IIFE is only necessary if one is using var i = 0 due to global scoping
+//     ((i) => {
+//       tasks[i]((result) => {
+//         resultArray[i] = result;
+//         counter++;
+//         if (counter === tasks.length) {
+//           return callback(resultArray);
+//         }
+//       });
+//     })(i);
+//   }
+// };
+
 const asyncMap = (tasks, callback) => {
-  const resultArray = [];
-  let counter = 0;
-  console.log(tasks);
+  const results = [];
+  let count = 0;
   for (let i = 0; i < tasks.length; i++) {
-    ((i) => {
-      tasks[i]((result) => {
-        resultArray[i] = result;
-        counter++;
-        if (counter === tasks.length) {
-          return callback(resultArray);
-        }
-      });
-    })(i);
+    tasks[i]((result) => {
+      results[i] = result;
+      count++;
+      if (count === tasks.length) {
+        return callback(results);
+      }
+    });
   }
 };
 
