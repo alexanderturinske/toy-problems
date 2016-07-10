24ea9d8e0fcd2c8c47e4b4010429db0d9024c577
Alexander James Turinke <a.turinske@gmail.com>
2016-07-09 19:17:22 -0700

    Implement a brute force solution to largestConsecutiveSum

--- Arrays/largestConsecutiveSum.js
+++ Arrays/largestConsecutiveSum.js
@@ -3,8 +3,23 @@
  * Given an array containing positive and negative numbers, find the largest consecutive sum
 **/
 
+// brute force
 const largestConsecutiveSum = (array) => {
-
+  let longest = 0;
+  let current = 0;
+  for (let i = 0; i < array.length; i++) {
+    current = array.slice(i).reduce( (acc, cur) => {
+      return acc + cur;
+    }, 0);
+    if (longest < current) {
+      longest = current;
+    }
+  }
+  let recursive = 0;
+  if (array.length) {
+    recursive = largestConsecutiveSum(array.slice(0, -1));
+  }
+  return Math.max(longest, recursive);
 };
 
 console.log(largestConsecutiveSum([]) === 0);
