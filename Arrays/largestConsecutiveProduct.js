eb54251647fb29c7720bb5b47bb66bfbeb6fe392
Alexander James Turinke <a.turinske@gmail.com>
2016-07-09 20:42:31 -0700

    Implement a smart solution to largestConsecutiveProduct

--- Arrays/largestConsecutiveProduct.js
+++ Arrays/largestConsecutiveProduct.js
@@ -4,7 +4,47 @@
 **/
 
 const largestConsecutiveProduct = (array) => {
-
+  let largest = 0;
+  let current = 0;
+  const negatives = [];
+  if (!array.length) {
+    return largest;
+  }
+  // if there is a zero
+  if (array.indexOf(0) >= 0) {
+    // split up the array and call recursivly
+    const first = largestConsecutiveProduct(array.slice(0, array.indexOf(0)));
+    const second = largestConsecutiveProduct(array.slice(array.indexOf(0) + 1));
+    if (largest < first || largest < second) {
+      largest = Math.max(first, second);
+    }
+  }
+  // find all the negative numbers
+  array.forEach((e, i) => {
+    if (e < 0) {
+      negatives.push(i);
+    }
+  });
+  // if there is an odd number of negative numbers
+  if (negatives.length % 2) {
+    // split up the array and call it recursively
+    const third = largestConsecutiveProduct(array.slice(0, negatives[negatives.length - 1]));
+    const fourth = largestConsecutiveProduct(array.slice(negatives[0] + 1));
+    if (largest < third || largest < fourth) {
+      largest = Math.max(third, fourth);
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (!current) {
+      current = array[i];
+    } else {
+      current = current * array[i];
+      if (largest < current) {
+        largest = current;
+      }
+    }
+  }
+  return largest;
 };
 
 console.log(largestConsecutiveProduct([]) === 0);
