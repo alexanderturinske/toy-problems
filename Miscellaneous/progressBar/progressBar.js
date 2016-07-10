dc7abd3c8bed10d8e35865d271b9c7cf1ecb0581
Alexander James Turinke <a.turinske@gmail.com>
2016-07-09 22:18:14 -0700

    Implement method to change width of div

---
+++ Miscellaneous/progressBar/progressBar.js
@@ -0,0 +1,8 @@
+var updateProgress = function (e) {
+  var input = document.querySelector('.input');
+  var val = input.value;
+  var progress = document.querySelector('.progress');
+  input.value = '';
+  document.querySelector('.container').style.width = val + 'px';
+  progress.style.width = val + 'px';
+};
