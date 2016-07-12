d88eaad0bfa875fc4140b06397c2c51f860c12ec
Alexander James Turinke <a.turinske@gmail.com>
2016-07-11 21:28:10 -0700

    Implement initial app file

---
+++ Angular/threeCities/app.js
@@ -0,0 +1,12 @@
+(function () {
+  angular
+    .module('ThreeButtons', [])
+    .controller('ButtonController', ButtonController);
+
+  ButtonController.$inject = [];
+
+  var ButtonController = function ButtonController() {
+    var vm = this;
+    
+  };
+})();
