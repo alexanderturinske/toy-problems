7965434b9b915e683b67c2736685ea338e1b7585
Alexander James Turinke <a.turinske@gmail.com>
2016-07-20 14:24:01 -0700

    Implement heapSerializeDeserialize skeleton

---
+++ Heaps/heapSerializeDeserialize.js
@@ -0,0 +1,35 @@
+'use strict';
+/**
+ * Description
+**/
+
+const heapSerialize = (heap) => {
+
+};
+
+const heapDeserialize = (heapArray) => {
+
+};
+
+// console.log(heapSerialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+// console.log(heapSerialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+// console.log(heapSerialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+// console.log(heapSerialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+
+// console.log(heapDeserialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+// console.log(heapDeserialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+// console.log(heapDeserialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+// console.log(heapDeserialize() === EXPECTED_OUTCOME);
+// // -> EXPECTED_OUTCOME
+
+module.exports = {
+  heapSerialize,
+  heapDeserialize,
+};
